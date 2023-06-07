f8ea9cb741f3a9d1c2766ba92b50a3842e2f8b0c
vincenzodivita <divitavincenzo@icloud.com>
2023-06-07 15:40:26 +0200

    Add Bootstrap

--- index.php
+++ index.php
@@ -49,25 +49,31 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Trova Hotel</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     </head>
     <body>
         
-            <table>
-                <tr>
-                    <th>Nome</th>
-                    <th>Descrizione</th>
-                    <th>Voto</th>
-                    <th>Distanza dal centro</th>
-                </tr><?php 
-
-                foreach ($hotels as $hotel) {?>
-                <tr>
-                    <th><?= $hotel["name"] ?></th>
-                    <th><?= $hotel["description"] ?></th>
-                    <th><?= $hotel["vote"] ?></th>
-                    <th><?= $hotel["distance_to_center"] ?></th> 
-                </tr><?php 
-                }?> 
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th class="col">Nome</th>
+                        <th class="col">Descrizione</th>
+                        <th class="col">Voto</th>
+                        <th class="col">Distanza dal centro</th>
+                    </tr>
+                </thead>
+                <tbody><?php
+                    foreach ($hotels as $hotel) {?>
+                        <tr>
+                            <td><?= $hotel["name"] ?></td>
+                            <td><?= $hotel["description"] ?></td>
+                            <td><?= $hotel["vote"] ?></td>
+                            <td><?= $hotel["distance_to_center"] ?></td> 
+                        </tr><?php 
+                    }?> 
+                </tbody>
+                
             </table>
+
     </body>
 </html>
