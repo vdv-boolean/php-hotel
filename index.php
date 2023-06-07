25b154485fa3c80be5146f4353cb6cd2f70325af
vincenzodivita <divitavincenzo@icloud.com>
2023-06-07 15:51:33 +0200

    Add parking column

--- index.php
+++ index.php
@@ -51,28 +51,30 @@
         <title>Trova Hotel</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     </head>
-    <body>
+    <body>  
         
             <table class="table">
                 <thead>
                     <tr>
                         <th class="col">Nome</th>
                         <th class="col">Descrizione</th>
+                        <th class="col">Parcheggio</th>
                         <th class="col">Voto</th>
                         <th class="col">Distanza dal centro</th>
                     </tr>
                 </thead>
                 <tbody><?php
-                    foreach ($hotels as $hotel) {?>
+                    foreach ($hotels as $hotel) {
+                        $with_park=$hotel['parking']? 'Si':'No';?>
                         <tr>
                             <td><?= $hotel["name"] ?></td>
                             <td><?= $hotel["description"] ?></td>
+                            <td><?= $with_park ?></td>
                             <td><?= $hotel["vote"] ?></td>
                             <td><?= $hotel["distance_to_center"] ?></td> 
                         </tr><?php 
                     }?> 
                 </tbody>
-                
             </table>
 
     </body>
