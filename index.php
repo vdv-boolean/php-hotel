f59fd2eda10bd81c9f4b51fb6a05795672e70821
vincenzodivita <divitavincenzo@icloud.com>
2023-06-07 15:57:53 +0200

    Add header title

--- index.php
+++ index.php
@@ -52,7 +52,10 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     </head>
     <body>  
-        
+        <div class="container">
+            <header class="d-flex justify-content-center m-4">
+                <h1>Trova l'hotel giusto per te!</h1>
+            </header>
             <table class="table">
                 <thead>
                     <tr>
@@ -76,6 +79,6 @@
                     }?> 
                 </tbody>
             </table>
-
+        </div>
     </body>
 </html>
